af23a2c9248c7fb682266e4f7d135914b74444be
Rasmus Kromann-Larsen <rkl@templafy.com>
2019-11-12 10:39:27 +0100

    Updates

---
+++ precache-manifest.e11dda7acc8f248e1a640145daf8ea18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881fecdc05eb6b7ef12b5a13b67757f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f34d6b237088bf369ad",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "0cd0beab6516e42acacc",
+    "url": "/static/js/2.d1afcee4.chunk.js"
+  },
+  {
+    "revision": "0f34d6b237088bf369ad",
+    "url": "/static/js/main.d1eab93d.chunk.js"
+  },
+  {
+    "revision": "c1ea5f656d057bae7d99",
+    "url": "/static/js/runtime-main.813d0afd.js"
+  }
+]);
