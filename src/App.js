c45367ff23ffa42568306f2523a7370acd823717
Rasmus Kromann-Larsen <rkl@templafy.com>
2019-11-12 09:57:09 +0100

    Implemented slack markdownifier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import slacifyMarkdown from 'slackify-markdown';
+import copy from 'copy-to-clipboard';
 
 function App() {
+  const [text, setText] = React.useState('');
+
+  function copyText() {
+    copy(slacifyMarkdown(text));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{background: '#555', color: '#fff', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <div style={{fontWeight: 'bold', padding: 10}}>Slack Markdownifier</div>
+        <div style={{padding: "10px 20px", background: '#333', cursor: 'pointer'}} onClick={copyText}>Copy Result</div>
+      </div>
+
+      <div className="App" style={{display: 'flex', width: '100%', height: '100vh', alignItems: 'stretch'}}>
+        <textarea style={{flex: 1}} value={text} onChange={e => setText(e.target.value)}></textarea>
+        <div style={{flex: 1, background: '#ddd'}}>{slacifyMarkdown(text)}</div>
+      </div>
     </div>
   );
 }
